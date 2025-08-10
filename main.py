bdad559727ac5d7f5668ecd427530f3493a108fa
Shreya Singh <shreyasingh.codes26@gmail.com>
2025-08-11 00:54:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+import pandas
+import random
+import time
+
+
+window = Tk()
+window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+window.title("Flashy")
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+canvas = Canvas(width=800,height=526)
+front_img = PhotoImage(file="images/card_front.png")
+back_img= PhotoImage(file="images/card_back.png")
+card_background= canvas.create_image(400,263,image=front_img)
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0,column=0,columnspan=2)
+card_title= canvas.create_text(400,150,text="",font=("Ariel",48,"italic"))
+card_word = canvas.create_text(400,263,text="",font=("Ariel",60,"bold"))
+
+def flip_card():
+    canvas.itemconfig(card_title,text="English")
+    canvas.itemconfig(card_word,text=current_card["English"])
+    canvas.itemconfig(card_background, image=back_img)
+
+flip_timer = window.after(3000,func=flip_card)
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title,text="French")
+    canvas.itemconfig(card_word,text=current_card["French"])
+    flip_timer= window.after(3000, func=flip_card)
+
+
+cross_image= PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image,command= next_card)
+unknown_button.grid(row=1,column=0)
+
+
+
+
+check_image= PhotoImage(file="images/right.png")
+known_button = Button(image=check_image,command=next_card)
+known_button.grid(row=1,column=1)
+
+
+
+
+next_card()
+
+
+
+
+
+
+window.mainloop()
